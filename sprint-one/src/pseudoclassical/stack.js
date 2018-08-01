33d8a1867ba8381b761926f6ab1bfa3cbc429770
Charlie Phan <charliegphan@gmail.com>
2018-08-01 16:22:25 -0700

    complete pseudoclassical stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -4,10 +4,19 @@ var Stack = function () {
 };
 
 Stack.prototype.push = function (value) {
-
+  this.counter++;
+  this.storage[this.counter] = value;
 };
 
-Stack.prototype.pop = function () { };
+Stack.prototype.pop = function () {
+  if (this.counter === 0) {
+    return 0;
+  }
+  var popped = this.storage[this.counter];
+  delete this.storage[this.counter];
+  this.counter--;
+  return popped;
+};
 
 Stack.prototype.size = function () {
   return Object.keys(this.storage).length;
