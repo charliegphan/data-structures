d270855eb7c8ba735dbb1fbd4d608604eebcfe08
Charlie Phan <charliegphan@gmail.com>
2018-08-01 16:16:40 -0700

    complete prototypal queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -7,12 +7,20 @@ var Queue = function () {
 
 var queueMethods = {};
 
-queueMethods.queue = function (value) {
-
+queueMethods.enqueue = function (value) {
+  this.storage[this.counter] = value;
+  this.counter++;
 };
 
 queueMethods.dequeue = function () {
+  if (Object.keys(this.storage).length === 0) {
+    return 0;
+  }
 
+  var dequeued = this.storage[Object.keys(this.storage)[0]];
+  delete this.storage[Object.keys(this.storage)[0]];
+
+  return dequeued;
 };
 
 queueMethods.size = function () {
