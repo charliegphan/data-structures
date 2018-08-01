137cac901ea28b30b765244f3f519e3d3cfd4e31
Charlie Phan <charliegphan@gmail.com>
2018-08-01 12:26:33 -0700

    begin queue, complete up to does not error

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,22 @@
-var Queue = function() {
+var Queue = function () {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  counter = 0;
   // Implement the methods below
 
-  someInstance.enqueue = function(value) {
+  someInstance.enqueue = function (value) {
+    storage[counter] = value;
+    counter++;
   };
 
-  someInstance.dequeue = function() {
+  someInstance.dequeue = function () {
+
   };
 
-  someInstance.size = function() {
+  someInstance.size = function () {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
