1fbe54533f6b9196bd1b8a5c47648458cdc5775c
Charlie Phan <charliegphan@gmail.com>
2018-08-01 15:12:46 -0700

    set up func shared queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,24 @@
-var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+var Queue = function () {
+  var instance = {};
+
+  instance.storage = {};
+  instance.push = queueMethods.push;
+  instance.pop = queueMethods.pop;
+  instance.size = queueMethods.size;
+
+  return instance;
 };
 
 var queueMethods = {};
 
+queueMethods.push = function () {
 
+};
+
+queueMethods.pop = function () {
+
+};
+
+queueMethods.size = function () {
+
+};
