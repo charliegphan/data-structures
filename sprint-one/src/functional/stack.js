ab350bc7ed09ea7a4dbe2a5c23757df01b3c54f6
Charlie Phan <charliegphan@gmail.com>
2018-08-01 11:50:09 -0700

    complete up to does not error when removing

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,23 @@
-var Stack = function() {
+var Stack = function () {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
-  someInstance.push = function(value) {
+  someInstance.push = function (value) {
+    // counter++;
+    someInstance[Object.keys(someInstance).length + 3] = value;
   };
 
-  someInstance.pop = function() {
+  someInstance.pop = function () {
+    delete someInstance[Object.keys(someInstance).length - 3];
+
   };
 
-  someInstance.size = function() {
+  someInstance.size = function () {
+
+    return Object.keys(someInstance).length - 3;
   };
 
   return someInstance;
