ea135597692ed64839367329c59be643065f7c2b
Charlie Phan <charliegphan@gmail.com>
2018-08-01 15:19:32 -0700

    complete functional shared queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -2,8 +2,9 @@ var Queue = function () {
   var instance = {};
 
   instance.storage = {};
-  instance.push = queueMethods.push;
-  instance.pop = queueMethods.pop;
+  instance.counter = 0;
+  instance.enqueue = queueMethods.enqueue;
+  instance.dequeue = queueMethods.dequeue;
   instance.size = queueMethods.size;
 
   return instance;
@@ -11,14 +12,21 @@ var Queue = function () {
 
 var queueMethods = {};
 
-queueMethods.push = function () {
-
+queueMethods.enqueue = function (value) {
+  this.storage[this.counter] = value;
+  this.counter++;
 };
 
-queueMethods.pop = function () {
+queueMethods.dequeue = function () {
+  if (Object.keys(this.storage).length === 0) {
+    return 0;
+  }
 
+  var dequeued = this.storage[Object.keys(this.storage)[0]];
+  delete this.storage[Object.keys(this.storage)[0]];
+  return dequeued;
 };
 
 queueMethods.size = function () {
-
+  return Object.keys(this.storage).length;
 };
