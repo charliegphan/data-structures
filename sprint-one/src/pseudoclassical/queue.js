5077ee52e2b2289b6bb21cdebd3c9cf93a8b319e
Charlie Phan <charliegphan@gmail.com>
2018-08-01 16:27:01 -0700

    complete pseuodoclassical queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -3,10 +3,20 @@ var Queue = function () {
   this.counter = 0;
 };
 
-Queue.prototype.enqueue = function (value) { };
+Queue.prototype.enqueue = function (value) {
+  this.storage[this.counter] = value;
+  this.counter++;
+};
 
 Queue.prototype.dequeue = function () {
+  if (Object.keys(this.storage).length === 0) {
+    return 0;
+  }
 
+  var dequeued = this.storage[Object.keys(this.storage)[0]];
+  delete this.storage[Object.keys(this.storage)[0]];
+
+  return dequeued;
 };
 
 Queue.prototype.size = function () {
