36666b272d87f9b14aa7039b714e4718f8c6a0b4
Charlie Phan <charliegphan@gmail.com>
2018-08-01 15:32:02 -0700

    set up prototypal stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,26 @@
-var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+var Stack = function () {
+  var stack = Object.create(Stack.prototype);
+  // stack.counter = 0;
+  // stack.storage = {};
+  return stack;
 };
 
-var stackMethods = {};
+var stackMethods = {}; // why need stack Methods object?
+
+Stack.prototype.counter = 0;
+Stack.prototype.storage = {};
+
+Stack.prototype.push = function (value) {
+
+};
+
+Stack.prototype.pop = function () {
+
+};
+
+Stack.prototype.size = function () {
+  return this.counter;
+};
+
 
 
