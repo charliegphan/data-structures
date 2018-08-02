069beba436089c2adcc2dbe904e2f932f3932f95
Charlie Phan <charliegphan@gmail.com>
2018-08-01 17:22:09 -0700

    complete es6 stack

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -5,15 +5,22 @@ class Stack {
   }
 
   push(value) {
-
+    this.counter++;
+    this.storage[this.counter] = value;
   }
 
   pop() {
-
+    if (this.counter === 0) {
+      return 0;
+    }
+    var popped = this.storage[this.counter];
+    delete this.storage[this.counter];
+    this.counter--;
+    return popped;
   }
 
   size() {
-
+    return this.counter;
   }
 
 }
