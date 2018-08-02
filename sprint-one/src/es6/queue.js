be10a73dc033ac6deb0b6b62b421d27941231385
Charlie Phan <charliegphan@gmail.com>
2018-08-01 17:28:58 -0700

    complete es6 queue

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,15 +2,22 @@ class Queue {
   constructor() {
     this.counter = 0;
     this.storage = {};
+    // this.keys = Object.keys(this.storage);
   }
 
   enqueue(value) {
     this.storage[this.counter] = value;
-    counter++;
+    this.counter++;
   }
 
-  deqeueue() {
+  dequeue() {
+    if (Object.keys(this.storage).length === 0) {
+      return 0;
+    }
 
+    var dequeued = this.storage[Object.keys(this.storage)[0]];
+    delete this.storage[Object.keys(this.storage)[0]];
+    return dequeued;
   }
 
   size() {
